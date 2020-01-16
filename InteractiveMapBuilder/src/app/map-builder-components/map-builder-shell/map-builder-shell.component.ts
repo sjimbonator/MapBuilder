6ab5d6878d79e1870af1f8849228a55ef7ffe9e0
sjimbonator <43266491+sjimbonator@users.noreply.github.com>
2020-01-16 22:21:36 +0100

    responsive
    Map-builder page now displays properly on mobile devices

--- InteractiveMapBuilder/src/app/map-builder-components/map-builder-shell/map-builder-shell.component.ts
+++ InteractiveMapBuilder/src/app/map-builder-components/map-builder-shell/map-builder-shell.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {BreakpointObserver, BreakpointState} from '@angular/cdk/layout';
 
 @Component({
   selector: 'app-map-builder-shell',
@@ -6,10 +7,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./map-builder-shell.component.css']
 })
 export class MapBuilderShellComponent implements OnInit {
+  
+  constructor(private breakpointObserver: BreakpointObserver) { }
 
-  constructor() { }
+  rowHeight : string;
+  mapColSpan : number;
+  toolColSpan : number;
 
   ngOnInit() {
+    this.breakpointObserver
+      .observe(['(min-width: 599px)'])
+      .subscribe((state: BreakpointState) => {
+        if (state.matches) {
+          this.mapColSpan = 4;
+          this.toolColSpan = 1;
+          this.rowHeight = " 2 : 5";
+        } else {
+          this.mapColSpan = 6;
+          this.toolColSpan = 3;
+          this.rowHeight = " 2 : 8";
+        }
+      });
   }
 
 }
